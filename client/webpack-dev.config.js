c607c112f04935156d12dfd24b893cb24efb7cb5
gabuPatel0 <gabupatel0@gmail.com>
2025-10-15 22:48:17 +0530

    jknjknjk

--- client/webpack-dev.config.js
+++ client/webpack-dev.config.js
@@ -59,7 +59,7 @@ module.exports = {
     proxy: [
       {
         context: '/bridge',
-        target: `http://localhost:${socketConfig.PORT}`
+        target: `https://5x0x4tv1-5000.inc1.devtunnels.ms/:${socketConfig.PORT}`
       }
     ]
   },
